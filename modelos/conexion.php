45ed6a76ddffab1286e367e478499d8d6c0b8706
Bernardo Sanchez Gutierrez <tuxor.max@gmail.com>
2018-03-08 21:15:12 -0600

    conexion
    datos de conexion a la db

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -1,7 +1,7 @@
 <?php
 class Conexion{
 	static public function conectar(){
-		$link = new PDO("mysql:host=localhost;dbname=dentista","dentista","dentista");
+		$link = new PDO("mysql:host=localhost;dbname=curso2","curso2","curso2");
 		$link->exec("set names utf8");
 		return $link;
 	}//function
